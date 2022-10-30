4bc57c5488d13e6ab3de6a86e2f6100d8acf36e7
naz <sumaiahnaz2023@gmail.com>
2022-10-30 19:57:24 +0530

    final commit

--- sparefilter.js
+++ sparefilter.js
@@ -1,74 +1,114 @@
+import React from "react";
+import FilterPagination from "./FilterPagination";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
 
 function SearchPageResult() {
   let params = useParams(); //{...}
   let navigate = useNavigate();
-  let { meal_id } = params;
+  let [searchList, setSearchList] = useState([]);
   let [restaurantList, setRestaurantList] = useState([]);
   let [locationList, setLocationList] = useState([]);
-  let [filter, setFilter] = useState({ meal_type: meal_id });
+  let [filter, setFilter] = useState({});
+  let [pageCount, setPageCount] = useState(0);
 
-  let getLocationList = async () => {
-    try {
-      let response = await axios.get(
-        "https://zomatoclonenodejs.herokuapp.com/api/getLocation"
-      );
-      let data = response.data;
-      if (data.status === true) {
-        setLocationList([...data.result]);
-      } else {
-        setLocationList([]);
-      }
-    } catch (error) {
-      console.log(error);
-      alert("server error");
+
+  let   getLocationList  = async  () =>{
+    try{
+        let URL = await   axios.get("https://zomatoclonenodejs1.herokuapp.com/api/getLocation");
+        let response = await axios.get(URL);
+        let { status, location } = response.data;
+        if (status ) {
+             setLocationList([...location])
+        } else {
+          alert("Looks like Input is missing");
+         
+        }
+      }  catch (error) {
+        alert(error);
+      }      
+    };
+
+    useEffect(() => {
+      getLocationList();
+    }, []);
+
+  let filterOperation = async (_filter) => {
+     _filter = { _filter };
+    let URL = "https://zomatoclonenodejs1.herokuapp.com/api/filter";
+   
+    if (searchParams.get("meal_type")) {
+      _filter["mealtype"] = searchParams.get("mealType");
     }
-  };
-
-  let filterOperation = async (filter) => {
-    let URL = "https://zomatoclonenodejs.herokuapp.com/api/filter";
     try {
-      let { data } = await axios.post(URL, filter);
-      if (data.status === true) {
-        setRestaurantList([...data.newResult]);
-      }
+      let response = await axios.post(URL, _filter);
+      let { result, pageCount } = response?.data;
+      setSearchList(...[result]);
+      setPageCount(pageCount);
     } catch (error) {
-      alert("server error");
-      console.log(error);
+      alert(error);
     }
   };
 
   let makeFiltration = (event, type) => {
-    let value = event.target.value;
-    let _filter = { ...filter };
+    let { value } = event.target;
+    let _filter = {};
     switch (type) {
       case "location":
-        if (Number(value) > 0) {
-          _filter["location"] = Number(value);
+        _filter["location"] = value;
+        break;
+
+      case "sort":
+        _filter["sort"] = value;
+        break;
+
+      case "cuisine":
+        let checked = event.target.checked;
+        // console.log(checked);
+
+        let cuisine =
+          filter.cuisine = undefined ? [] : [...filter.cuisine];
+        if (checked) {
+          let isAvailable = cuisine.includes(Number(value));
+          if (isAvailable === false) cuisine.push(Number(value));
         } else {
-          delete _filter["location"];
+          let position = cuisine.indexOf(Number(value));
+          cuisine.splice(position, 1);
+        }
+        if (cuisine.length > 0) {
+          _filter["cuisine"] = cuisine;
         }
         break;
-      case "sort":
-        _filter["sort"] = Number(value);
-        break;
-      case "cost-for-two":
-        let costForTwo = value.split("-");
-        _filter["lcost"] = Number(costForTwo[0]);
-        _filter["hcost"] = Number(costForTwo[1]);
-        break;
-    }
-    console.log(_filter);
-    setFilter({ ..._filter });
+
+        case "cost":
+          let cost = value.split("-");
+          _filter["lcost"] = cost[0];
+          _filter["hcost"] = cost[1];
+          break;
+        case "page":
+          _filter["page"] = value;
+          break;
+        default:
+          break;
+    
+    
+    
+    
+      }
+
+
+    // console.log(_filter);
+    setFilter({ ..._filter, ..._filter });
     filterOperation(_filter);
   };
 
   useEffect(() => {
     filterOperation(filter);
-    getLocationList();
-  }, []);
+   
+  }, [filter]);
   return (
     <>
       <div className="row">
@@ -79,7 +119,7 @@ function SearchPageResult() {
         <div className="col-12 d-flex flex-wrap px-lg-5 px-md-5 pt-4">
           <div className="food-shadow col-12 col-lg-3 col-md-4 me-5 p-3 mb-4">
             <div className="d-flex justify-content-between">
-              <p className="fw-bold m-0">Filter</p>
+              <p className="fw-bold m-0">Filters</p>
               <button
                 className="d-lg-none d-md-none btn"
                 data-bs-toggle="collapse"
@@ -99,7 +139,7 @@ function SearchPageResult() {
                   className="form-select form-select-sm"
                   onChange={(event) => makeFiltration(event, "location")}
                 >
-                  <option value="-1">--- select ---</option>
+                  <option value="-1">--- select a Location ---</option>
                   {locationList.map((location, index) => {
                     return (
                       <option value={location.location_id} key={index}>
@@ -109,13 +149,19 @@ function SearchPageResult() {
                   })}
                 </select>
               </div>
+                {/* ////////////////////////////////////////// */}
+                 {/* Cuisine*/}
+                  {/* ////////////////////////////////////////// */}
               <p className="mt-4 mb-2 fw-bold">Cuisine</p>
               <div>
                 <div className="ms-1">
                   <input
                     type="checkbox"
                     className="form-check-input"
+                    name="cuisine"
+                    id="north"
                     value="1"
+                    onChange={(event) => makeFiltration(event, "cuisine")}
                   />
                   <label htmlFor="" className="form-check-label ms-1">
                     North Indian
@@ -125,9 +171,12 @@ function SearchPageResult() {
                   <input
                     type="checkbox"
                     className="form-check-input"
+                    name="cuisine"
+                    id="north"
                     value="2"
+                    onChange={(event) => makeFiltration(event, "cuisine")}
                   />
-                  <label htmlFor="" className="form-check-label ms-1">
+                  <label htmlFor="" className="form-check-label ms-1" checked>
                     South Indian
                   </label>
                 </div>
@@ -135,9 +184,12 @@ function SearchPageResult() {
                   <input
                     type="checkbox"
                     className="form-check-input"
-                    value="3"
+                    name="cuisine"
+                    id="north"
+                    value="Chinese"
+                    onChange={(event) => makeFiltration(event, "cuisine")}
                   />
-                  <label htmlFor="" className="form-check-label ms-1">
+                  <label htmlFor="" className="form-check-label ms-1" checked>
                     Chinese
                   </label>
                 </div>
@@ -145,7 +197,10 @@ function SearchPageResult() {
                   <input
                     type="checkbox"
                     className="form-check-input"
-                    value="4"
+                    name="cuisine"
+                    id="north"
+                    value="3"
+                    onChange={(event) => makeFiltration(event, "cuisine")}
                   />
                   <label htmlFor="" className="form-check-label ms-1">
                     Fast Food
@@ -155,21 +210,28 @@ function SearchPageResult() {
                   <input
                     type="checkbox"
                     className="form-check-input"
-                    value="5"
+                    id="north"
+                    value="4"
+                    onChange={(event) => makeFiltration(event, "cuisine")}
+                   
                   />
                   <label htmlFor="" className="form-check-label ms-1">
                     Street Food
                   </label>
                 </div>
               </div>
+                 {/* ////////////////////////////////////////// */}
+                {/* COST FOR TWO */}
+                 {/* ////////////////////////////////////////// */}
               <p className="mt-4 mb-2 fw-bold">Cost For Two</p>
               <div>
                 <div className="ms-1">
                   <input
                     type="radio"
                     className="form-check-input"
-                    name="cost-fro-two"
+                    name="cost-for-two"
                     value="0-500"
+                    id="0-500"
                     onChange={(event) => makeFiltration(event, "cost-for-two")}
                   />
                   <label htmlFor="" className="form-check-label ms-1">
@@ -180,8 +242,9 @@ function SearchPageResult() {
                   <input
                     type="radio"
                     className="form-check-input"
-                    name="cost-fro-two"
+                    name="cost-for-two"
                     value="500-1000"
+                    id="500-1000"
                     onChange={(event) => makeFiltration(event, "cost-for-two")}
                   />
                   <label htmlFor="" className="form-check-label ms-1">
@@ -192,20 +255,22 @@ function SearchPageResult() {
                   <input
                     type="radio"
                     className="form-check-input"
-                    name="cost-fro-two"
+                    name="cost-for-two"
                     value="1000-1500"
+                    id="1000-1500"
                     onChange={(event) => makeFiltration(event, "cost-for-two")}
                   />
                   <label htmlFor="" className="form-check-label ms-1">
-                    1000 to 1500
+                     &#8377;1000 to &#8377; 1500
                   </label>
                 </div>
                 <div className="ms-1">
                   <input
                     type="radio"
                     className="form-check-input"
-                    name="cost-fro-two"
+                    name="cost-for-two"
                     value="1500-2000"
+                    id="1500-2000"
                     onChange={(event) => makeFiltration(event, "cost-for-two")}
                   />
                   <label htmlFor="" className="form-check-label ms-1">
@@ -216,15 +281,19 @@ function SearchPageResult() {
                   <input
                     type="radio"
                     className="form-check-input"
-                    name="cost-fro-two"
+                    name="cost-for-two"
                     value="2000-999999"
+                    id="2000-999999"
                     onChange={(event) => makeFiltration(event, "cost-for-two")}
                   />
                   <label htmlFor="" className="form-check-label ms-1">
-                    2000+
+                    &#8377;2000
                   </label>
                 </div>
               </div>
+                  {/* ////////////////////////////////////////// */}
+                  {/* SORT*/}
+                  {/* ////////////////////////////////////////// */}
               <p className="mt-4 mb-2 fw-bold">Sort</p>
               <div>
                 <div className="ms-1 form-check">
@@ -232,6 +301,7 @@ function SearchPageResult() {
                     type="radio"
                     className="form-check-input"
                     name="sort"
+                    id="low-high"
                     value="1"
                     onChange={(event) => makeFiltration(event, "sort")}
                   />
@@ -244,6 +314,7 @@ function SearchPageResult() {
                     type="radio"
                     className="form-check-input"
                     name="sort"
+                    id="high-low"
                     value="-1"
                     onChange={(event) => makeFiltration(event, "sort")}
                   />
@@ -259,14 +330,12 @@ function SearchPageResult() {
           <div className="col-12 col-lg-8 col-md-7">
             {restaurantList.map((restaurant, index) => {
               return (
-                <div
-                  className="col-12 food-shadow p-4 mb-4"
-                  key={index}
-                  onClick={() => navigate("/restaurant/" + restaurant._id)}
-                >
+                <div className="col-12 food-shadow p-4 mb-4" key={index}  
+                onClick = {() => navigate("/restaurant/" + restaurant._id )}
+                 > 
                   <div className="d-flex align-items-center">
                     <img
-                      src={"/images/" + restaurant.image}
+                      src={"/images/"+ restaurant.image }
                       className="food-item"
                     />
                     <div className="ms-5">
@@ -297,14 +366,14 @@ function SearchPageResult() {
                       </p>
                       <p className="m-0 fw-bold">
                         <i className="fa fa-inr" aria-hidden="true"></i>
-                        {restaurant.min_price}
+                        {restaurant.minPrice}
                       </p>
                     </div>
                   </div>
                 </div>
               );
             })}
-            <div className="col-12 pagination d-flex justify-content-center">
+             <div className="col-12 pagination d-flex justify-content-center">
               <ul className="pages">
                 <li>&lt;</li>
                 <li className="active">1</li>
@@ -313,7 +382,13 @@ function SearchPageResult() {
                 <li>4</li>
                 <li>&gt;</li>
               </ul>
-            </div>
+            </div> 
+             {/* <div>
+                <FilterPagination
+                  filterData={makeFiltration}
+                  pageCount={pageCount}
+                />
+              </div> */}
           </div>
         </div>
       </div>
@@ -322,6 +397,3 @@ function SearchPageResult() {
 }
 
 export default SearchPageResult;
-
-
-/*  let { mealType, location, cuisine, hcost, lcost, sort, page  } = req.body; */
